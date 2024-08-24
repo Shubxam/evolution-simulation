7279514baca206da4fd03a71c643e0c08763286c
Shubham Soni <shubxam@gmail.com>
2024-08-24 15:05:48 +0530

    add test cases and generate random networks using seed

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{Rng, RngCore};
 
 #[derive(Debug)]
 pub struct Network{
@@ -15,12 +15,12 @@ impl Network {
         Self {layers}
     }
 
-    pub fn random(layers: &[LayerTopology]) -> Self {
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
 
         assert!(layers.len() > 1);
 
         let layers = layers.windows(2)
-                            .map(|adjacent_layers| Layer::random(adjacent_layers[0].neurons, adjacent_layers[1].neurons))
+                            .map(|adjacent_layers| Layer::random(rng, adjacent_layers[0].neurons, adjacent_layers[1].neurons))
                             .collect();
 
         Self {layers: layers}
@@ -34,15 +34,15 @@ impl Network {
 }
 
 #[derive(Debug)]
-struct Layer{
+pub struct Layer{
     neurons: Vec<Neuron>
 }
 
 impl Layer {
 
-    fn random(input_size: usize, output_size: usize) -> Self{
+    fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self{
         let neurons = (0..output_size)
-                            .map(|_output_neuron_id| Neuron::random(input_size))
+                            .map(|_output_neuron_id| Neuron::random(rng, input_size))
                             .collect();
 
         Self {neurons: neurons}
@@ -67,9 +67,7 @@ struct Neuron{
 
 impl Neuron {
 
-    fn random(input_size: usize) -> Self {
-
-        let mut rng = rand::thread_rng();
+    fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
         
         let bias = rng.gen_range(-1.0..=1.0);
         let weights = (0..input_size)
@@ -89,5 +87,82 @@ impl Neuron {
                                 .sum::<f32>();
 
         (self.bias + output).max(0.0)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use approx::assert_relative_eq;
+
+    mod neuron_tests {
+        use super::*;
+
+        #[test]
+        fn random(){
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let neuron = Neuron::random(&mut rng, 4);
+    
+            assert_relative_eq!(neuron.bias, -0.6255188);
+            assert_relative_eq!(neuron.weights.as_slice(), [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref());
+        }
+    
+        #[test]
+        fn propogate(){
+            let neuron = Neuron {
+                bias : 0.5,
+                weights: vec![-0.3, 0.8]
+            };
+    
+            // Ensure ReLU function works
+            assert_relative_eq!(neuron.propogate(&[2.2, 0.9]), 0.56);
+        }
+    }
+
+    mod layer_tests {
+        use super::*;
+
+        #[test]
+        fn random(){
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let neurons_count = 3;
+            let layer = Layer::random(&mut rng, 4, neurons_count);
+
+            assert_eq!(layer.neurons.len(), neurons_count);
+        }
+
+        #[test]
+        fn propogate(){
+            let layer = Layer {
+                neurons : vec![Neuron {bias: 0.5, weights: vec![-0.3, 0.8]},
+                            Neuron {bias: 0.1, weights: vec![0.11, -1.5]},
+                            Neuron {bias: -0.15, weights: vec![-0.11, 1.5]},
+                        ]
+            };
+
+            // assert_relative_eq!(layer.propogate((&[2.2, 0.9]).to_vec()),
+            //     vec![0.56, 0.0, 0.958]);
+            assert_eq!(layer.propogate([2.2, 0.9].to_vec()).len(), 3);
+        }
+
+    }
+
+    mod function_tests {
+        use super::*;
+
+        #[test]
+        fn random(){
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let layer_struct = [LayerTopology{neurons: 2}, LayerTopology{neurons:3}, LayerTopology{neurons:2}];
+            let network = Network::random(&mut rng, &layer_struct);
+            assert_eq!(network.layers.len(), layer_struct.len() - 1);
+        }
+
+        // fn propogate(){
+        //     todo!();
+        // }
     }
 }
