9b0b62cb9c8bc3d45d2ad419562bf75009c15735
Shubham Soni <shubxam@gmail.com>
2024-12-06 18:48:26 +0530

    refactor code

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -2,22 +2,6 @@ use rand::seq::SliceRandom;
 use rand::{Rng, RngCore};
 use std::ops::Index;
 
-// this struct is generic over trait S
-pub struct GeneticAlgorithm<S, C, M> {
-    selection_method: S,
-    crossover_method: C,
-    mutation_method: M,
-}
-
-pub trait Individual {
-    fn create(chromosome: Chromosome) -> Self;
-    fn fitness(&self) -> f32;
-    fn chromosome(&self) -> &Chromosome;
-}
-
-pub struct RouletteWheelSelection;
-
-pub struct UniformCrossover;
 
 #[derive(Clone, Debug)]
 pub struct Chromosome{
@@ -28,12 +12,12 @@ impl Chromosome {
     pub fn len(&self) -> usize {
         self.genes.len()
     }
-
+    
     // function returns a type which implements Iterator trait generic over f32
     pub fn iter(&self) -> impl Iterator<Item = &f32> {
         self.genes.iter()
     }
-
+    
     pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut f32> {
         self.genes.iter_mut()
     }
@@ -44,7 +28,7 @@ impl Index<usize> for Chromosome {
     
     // type alias for Output associated type
     type Output = f32;
-
+    
     fn index(&self, index: usize) -> &Self::Output {
         &self.genes[index]
     }
@@ -58,7 +42,7 @@ impl FromIterator<f32> for Chromosome {
             genes: iter.into_iter().collect(),
         }
     }
-
+    
 }
 
 // implementation of IntoIterator trait for Chromosome struct
@@ -66,11 +50,14 @@ impl FromIterator<f32> for Chromosome {
 impl IntoIterator for Chromosome {
     type Item = f32;
     type IntoIter = std::vec::IntoIter<f32>;
-
+    
     fn into_iter(self) -> Self::IntoIter {
         self.genes.into_iter()
     }
 }
+
+
+pub struct RouletteWheelSelection;
 
 /// A trait implementing selection method that selects individuals based on their fitness.
 pub trait SelectionMethod {
@@ -78,12 +65,40 @@ pub trait SelectionMethod {
     // function works with any generic type I that implements Individual trait.
     fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
     where
-        I: Individual;
+    I: Individual;
 }
+
+// Implementation of Selection Method trait for Type Roulette Wheel Selection.
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+    I: Individual,
+    {
+        population
+        .choose_weighted(rng, |individual| individual.fitness())
+        .expect("got an empty population")
+    }
+}
+
+pub struct UniformCrossover;
 
 pub trait CrossoverMethod {
     fn crossover(&self, rng: &mut dyn RngCore, parent_a: &Chromosome, parent_b: &Chromosome) -> Chromosome;
 }
+
+impl CrossoverMethod for UniformCrossover {
+    fn crossover(&self, rng: &mut dyn RngCore, parent_a: &Chromosome, parent_b: &Chromosome) -> Chromosome {
+        assert_eq!(parent_a.len(), parent_b.len());
+        
+        parent_a
+        .iter()
+        .zip(parent_b.iter())
+        .map(|(&a, &b)| if rng.gen_bool(0.5) {a} else {b})
+        .collect()
+        
+    }
+}
+
 
 pub trait MutationMethod {
     fn mutation(&self, rng: &mut dyn RngCore, child: &mut Chromosome);
@@ -119,30 +134,13 @@ impl MutationMethod for GaussianMutation {
     }
 }
 
-// Implementation of Selection Method trait for Type Roulette Wheel Selection.
-impl SelectionMethod for RouletteWheelSelection {
-    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
-    where
-        I: Individual,
-    {
-        population
-            .choose_weighted(rng, |individual| individual.fitness())
-            .expect("got an empty population")
-    }
+// this struct is generic over trait S
+pub struct GeneticAlgorithm<S, C, M> {
+    selection_method: S,
+    crossover_method: C,
+    mutation_method: M,
 }
 
-impl CrossoverMethod for UniformCrossover {
-    fn crossover(&self, rng: &mut dyn RngCore, parent_a: &Chromosome, parent_b: &Chromosome) -> Chromosome {
-        assert_eq!(parent_a.len(), parent_b.len());
-
-        parent_a
-        .iter()
-        .zip(parent_b.iter())
-        .map(|(&a, &b)| if rng.gen_bool(0.5) {a} else {b})
-        .collect()
-
-    }
-}
 
 impl<S, C, M> GeneticAlgorithm<S, C, M> 
 where 
@@ -196,32 +194,6 @@ mod tests {
     use std::collections::BTreeMap;
     use std::iter::FromIterator;
 
-    #[test]
-    fn roulette_wheel_selection() {
-        let mut rng = ChaCha8Rng::from_seed(Default::default());
-        let population = vec![
-            TestIndividual::new(2.0),
-            TestIndividual::new(1.0),
-            TestIndividual::new(4.0),
-            TestIndividual::new(3.0),
-        ];
-
-        let mut actual_histogram = BTreeMap::new();
-
-        for _ in 0..1000 {
-            // sample a random fitness score
-            let fitness = RouletteWheelSelection
-                .select(&mut rng, &population)
-                .fitness() as i32;
-
-            // increase the count
-            *actual_histogram.entry(fitness).or_insert(0) += 1;
-        }
-
-        let expected_histogram = BTreeMap::from_iter([(1, 98), (2, 202), (3, 278), (4, 422)]);
-        assert_eq!(actual_histogram, expected_histogram);
-    }
-
     #[derive(Clone, Debug, PartialEq)]
     pub enum TestIndividual {
         // tests that require access to chromosomes
@@ -263,6 +235,35 @@ mod tests {
             }
         }
     }
+
+
+    #[test]
+    fn roulette_wheel_selection() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+
+        let mut actual_histogram = BTreeMap::new();
+
+        for _ in 0..1000 {
+            // sample a random fitness score
+            let fitness = RouletteWheelSelection
+                .select(&mut rng, &population)
+                .fitness() as i32;
+
+            // increase the count
+            *actual_histogram.entry(fitness).or_insert(0) += 1;
+        }
+
+        let expected_histogram = BTreeMap::from_iter([(1, 98), (2, 202), (3, 278), (4, 422)]);
+        assert_eq!(actual_histogram, expected_histogram);
+    }
+
+    
 
     #[test]
     fn uniform_crossover() {
